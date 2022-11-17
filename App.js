e29676479d4d67cb989e7dbcba43c0211ad4f3ad
Andrey Yurlov <yurlovandrew@gmail.com>
2022-11-17 15:53:50 -0600

    init

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
+import * as eva from '@eva-design/eva';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import { ApplicationProvider, Text } from '@ui-kitten/components';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ApplicationProvider {...eva} theme={eva.light}>
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </ApplicationProvider>
   );
 }
 
